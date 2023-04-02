b3f35b1fd427006d1e8dc7a32d0e714e58efa7d0
viswa_global <viswamsd9100@gmail.com>
2023-04-02 22:16:14 +0530

    first commit

---
+++ routes/facultyAction.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const {
+   getPendingApplication,
+  getAllApplications,
+  getSingleApplicationForm,
+  updateStatusOfLeaveApplication,
+  getStudentInfo,
+  deleteAllApplicationForm,
+} = require("../controller/facultyActionController");
+const requireAuth = require("../middleware/FacultyrequireAuth");
+const router = express.Router();
+
+// require auth for all routes
+router.use(requireAuth);
+
+// GET all pending req for history
+router.get("/pendingapplication", getPendingApplication);
+
+// GET all req handled by faculty
+router.get("/allapplication", getAllApplications);
+
+// GET a single form
+router.get("/application/:id", getSingleApplicationForm);
+
+// change status and write comment for an application
+router.post("/updatestatus", updateStatusOfLeaveApplication);
+
+// GET student details
+router.get("/getstudent/:id", getStudentInfo);
+
+// DELETE all application application form under faculty
+router.post("/deleteallapplication", deleteAllApplicationForm);
+
+module.exports = router;
