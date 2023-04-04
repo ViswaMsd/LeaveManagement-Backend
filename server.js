b6aca76da0292bd97a210890bdd4db269358da17
Viswanath Reddy <80374810+ViswaMsd@users.noreply.github.com>
2023-04-04 08:05:05 +0530

    Added Cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const studentActionRoutes = require("./routes/studentAction");
 const studentUserRoutes = require("./routes/studentUser");
 const facultyUserRoutes = require("./routes/facultyUser");
@@ -8,6 +9,7 @@ const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
